a2391533a3f856ada0667c8859e61e117d770dc3
mattiatealdi <mattiatealdi@gmail.com>
2021-05-27 12:40:30 +0200

    milestone_2_completato

--- src/components/Film.vue
+++ src/components/Film.vue
@@ -3,7 +3,8 @@
       <ul>
           <li v-if="film.name == undefined">Titolo: {{ film.title }} </li> <li v-if="film.title == undefined ">Titolo: {{ film.name }}</li>
           <li v-if="film.original_name == undefined">Titolo originale: {{ film.original_title}}</li><li v-if="film.original_title == undefined "> Titolo originale: {{ film.original_name }}</li>
-          <li>Lingua: {{ film.original_language }}</li>
+          <li v-if="film.original_language == 'it'">Lingua: {{ film.original_language }} <country-flag country='it' size='small'/></li>
+          <li v-if="film.original_language =='en'">Lingua: {{film.original_language}} <country-flag country='gb' size='small'/> </li>
           <li>Voto: {{film.vote_average}} </li>
       </ul>
   </div>
